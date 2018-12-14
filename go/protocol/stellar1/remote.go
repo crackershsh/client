02478fa0b9098ededa4531f737bcd9930e13e8a0
Michał Zochniak <michal@zapu.net>
2018-12-14 14:34:47 +0100

    Add remote protocol for GetInflationDestination

--- go/protocol/stellar1/remote.go
+++ go/protocol/stellar1/remote.go
@@ -726,6 +726,11 @@ type SetInflationDestinationArg struct {
 	SignedTransaction string               `codec:"signedTransaction" json:"signedTransaction"`
 }
 
+type GetInflationDestinationArg struct {
+	Caller    keybase1.UserVersion `codec:"caller" json:"caller"`
+	AccountID AccountID            `codec:"accountID" json:"accountID"`
+}
+
 type PingArg struct {
 }
 
@@ -748,6 +753,7 @@ type RemoteInterface interface {
 	RequestDetails(context.Context, RequestDetailsArg) (RequestDetails, error)
 	CancelRequest(context.Context, CancelRequestArg) error
 	SetInflationDestination(context.Context, SetInflationDestinationArg) error
+	GetInflationDestination(context.Context, GetInflationDestinationArg) (*AccountID, error)
 	Ping(context.Context) (string, error)
 }
 
@@ -1025,6 +1031,21 @@ func RemoteProtocol(i RemoteInterface) rpc.Protocol {
 					return
 				},
 			},
+			"getInflationDestination": {
+				MakeArg: func() interface{} {
+					var ret [1]GetInflationDestinationArg
+					return &ret
+				},
+				Handler: func(ctx context.Context, args interface{}) (ret interface{}, err error) {
+					typedArgs, ok := args.(*[1]GetInflationDestinationArg)
+					if !ok {
+						err = rpc.NewTypeError((*[1]GetInflationDestinationArg)(nil), args)
+						return
+					}
+					ret, err = i.GetInflationDestination(ctx, typedArgs[0])
+					return
+				},
+			},
 			"ping": {
 				MakeArg: func() interface{} {
 					var ret [1]PingArg
@@ -1132,6 +1153,11 @@ func (c RemoteClient) CancelRequest(ctx context.Context, __arg CancelRequestArg)
 
 func (c RemoteClient) SetInflationDestination(ctx context.Context, __arg SetInflationDestinationArg) (err error) {
 	err = c.Cli.Call(ctx, "stellar.1.remote.setInflationDestination", []interface{}{__arg}, nil)
+	return
+}
+
+func (c RemoteClient) GetInflationDestination(ctx context.Context, __arg GetInflationDestinationArg) (res *AccountID, err error) {
+	err = c.Cli.Call(ctx, "stellar.1.remote.getInflationDestination", []interface{}{__arg}, &res)
 	return
 }
 
